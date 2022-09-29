427ff2c382ebfd2586be4e3be3f019bc58c1e757
unknown <janouser11@gmail.com>
2022-09-29 09:28:24 -0600

    Added backend validation

---
+++ amplify/backend/function/ValidateUnitConversionHandler/src/constants.js
@@ -0,0 +1,18 @@
+// temperatures
+const KELVIN = "kelvin";
+const CELSIUS = "celsius";
+const FAHRENHEIT = "fahrenheit";
+const RANKINE = "rankine";
+
+// volumes
+const LITERS = "liters";
+const TABLESPOONS = "tablespoons";
+const CUBIC_INCHES = "cubic inches";
+const CUPS = "cups";
+const CUBIC_FEET = "cubic feet";
+const GALLONS = "gallons";
+
+module.exports = Object.freeze({
+  TEMPERATURES: [KELVIN, CELSIUS, FAHRENHEIT, RANKINE],
+  VOLUMES: [LITERS, TABLESPOONS, CUBIC_INCHES, CUPS, CUBIC_FEET, GALLONS],
+});
